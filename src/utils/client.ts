c24717a4af3e4caad7827ab7dff2ce05d93c1d54
DxxxHxx <donghun1163@gmail.com>
2025-06-20 14:30:22 +0900

    feat: 토큰 만료 시 로그아웃

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { getAccessToken } from "./getToken";
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "../constants/constants";
 
 const client = axios.create({
   baseURL: "/api",
@@ -16,6 +17,19 @@ client.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
+  }
+);
+
+client.interceptors.response.use(
+  (res) => res,
+  async (e: AxiosError) => {
+    if (e.response?.status === 401) {
+      alert("로그인 시간이 만료되었습니다. 다시 로그인해주세요.");
+      [ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY].forEach((token) =>
+        localStorage.removeItem(token)
+      );
+      location.pathname = "/signin";
+    }
   }
 );
 
