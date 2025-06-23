f91c08791672e8e2f733b21315d686e5f1e5ace6
DxxxHxx <donghun1163@gmail.com>
2025-06-23 18:59:56 +0900

    chore: README update

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+/**
+ * staleTime
+ * (1000 ms * 60) === 1분,
+ * (1000 ms * 60) * 5 === 5분
+ */
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: { staleTime: 5 * 60 * 1000, gcTime: 10 * 60 * 1000 },
